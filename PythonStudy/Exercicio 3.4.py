e08ecc9d205ddd64ab8d637c5db56bd5afe8fb49
HayatoG <guilherme.ryders@gmail.com>
2018-10-31 21:49:46 -0300

    Adicionando exercícios de estudo Python

---
+++ PythonStudy/Exercicio 3.4.py
@@ -0,0 +1,5 @@
+print("Exercicio 3.4")
+
+salario = 1000
+
+salario > 1200
---
+++ PythonStudy/Exercicio 3.6.py
@@ -0,0 +1,7 @@
+print("Exercicio 3.6")
+
+materia1 = 7
+materia2 = 9
+materia3 = 4
+
+materia1 > 7 and materia2 > 7 and materia3 > 7
